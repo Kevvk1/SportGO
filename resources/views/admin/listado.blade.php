6b03a22ac25bdf859c0f7be35a7b9b02084e9465
Kevin <castellanokevin@icloud.com>
2024-08-18 20:21:36 -0300

    Reordenamiento de archivos

--- resources/views/admin/listado.blade.php
+++ resources/views/admin/listado.blade.php
@@ -6,58 +6,77 @@
     @if($productos->isEmpty())
         <p>No hay productos publicados.</p>
     @else
-    <div class="container text-center" style="margin-top: 1em;">
-        <div class="row">
-            @foreach($productos as $producto)                    
-                <div class="row" style="border: 1px solid gray; border-radius: 25px;">
+
+
+    <div class="table-responsive mt-5">
+        <table class="table">
+            <thead>
+                <tr style="background-color: #d9db26 !important;">
+                        <th scope="col">Codigo producto</th>
+                        <th scope="col">Imagen</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Descripcion</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Stock</th>
+                        <th scope="col">Cantidad en Stock</th>
+                        <th scope="col">Creada</th>
+                        <th scope="col">Ultima actualizacion</th>
+                        <th></th>
+                        <th></th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach($productos as $producto)  
+                <tr>
+                    <th scope="row">{{$producto -> codigo_producto}}</th>
                     
-                    <div class="col-3">
-                        <img src="{{ asset(Storage::url($producto['imagen'])) }}" class="img-fluid" style="height:80%; width:80%;"> 
-                    </div>
+                    <td><img src="{{ asset($producto->imagen) }}" alt="Imagen del producto" width="100" height="100"></td>
+                    <td>{{$producto -> nombre}}</td>
+                    <td>{{$producto -> descripcion}}</td>
+                    <td>{{$producto -> precio}}</td>
 
-                    <div class="col-6">
-                        <div class="row">
-                            <div class="col-12">
-                                <p>{{ $producto['nombre'] }}</p>
-                                <p>Stock disponible: {{ $producto['stock_disponible'] }} unidades</p>
-                            </div>      
-                        </div>
-                    </div>
+                    <td>{{ $producto -> stock_disponible > 0 ? 'Sí' : 'No' }}</td>
 
-                    <div class="col-3 align-self-center">
-                        <h3>${{ $producto['precio'] }}</h3>
-                    </div>
+                    <td>{{$producto -> stock_disponible > 0 ? $producto -> stock_disponible : '0' }}</td>
 
+                    <td>{{$producto -> created_at }}</td>
 
-                    <div class="col-6">
+                    <td>{{$producto -> updated_at }}</td>
 
-                        <!-- Campo oculto para código del producto -->
-                        <input type="hidden" name="codigo_producto" value="{{ $producto->codigo_producto }}">
-                        <!-- Campo oculto para nombre del producto -->
-                        <input type="hidden" name="nombre_producto" value="{{ $producto->nombre }}">
-                        <!-- Campo oculto para precio del producto -->
-                        <input type="hidden" name="precio_producto" value="{{ $producto->precio }}">
-                        <!-- Campo oculto para imagen del producto -->
-                        <input type="hidden" name="imagen_producto" value="{{ $producto->imagen }}">
-                        <!-- Campo oculto para stock del producto -->
-                        <input type="hidden" name="stock_producto" value="{{ $producto->stock_disponible }}">
+                    <td>
 
-                        <button class="btn" id="boton-modificar-producto" style="border-radius: 10px; background-color: yellow;" onclick="ModificarProducto( {{ $producto->codigo_producto }} )">Modificar</button>
-                    </div>
-                    
-                    <div class="col-6">
                         <form id="deleteProduct-form" action="{{ route('producto.eliminar') }}" method="POST">
                             @csrf
                             <!-- Campo oculto para código del producto -->
                             <input type="hidden" name="codigo_producto" value="{{ $producto->codigo_producto }}">
-                            <button type="submit" class="btn" style="border-radius: 10px; background-color: lightcoral;">Eliminar</button>
-                        </form>
-                    </div>
-                    
-                </div>
+                            <button type="submit" class="btn" style="border-radius: 10px; background-color: lightcoral;">
 
-            @endforeach
-        </div>  
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                                </svg>
+
+                            </button>
+                        </form>
+                        
+
+                    </td>
+
+                    <td>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="48" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16" onclick="ModificarProducto('{{ $producto->codigo_producto }}')" style="border-radius: 10px; background-color: #d9db26;">
+                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                        </svg>
+
+                    </td>
+                    
+                </tr>
+                
+                @endforeach
+            </tbody>
+        </table>
     </div>
 
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'SportGO - Iniciar sesión')
 
 @section('main')
-    <div class="container-fluid">
+    <div class="container">
         
         @if(session('success'))
            <h2 style="text-align: center;">{{ session('success') }}</h2>
@@ -13,9 +13,9 @@
 
         <form class="form-login" method="POST" action="{{ route('process.user') }}">
             @csrf
-            <div class="row align-items-center" id="loginform">
-                <div class="col-lg-6">
-                    <div class="row">
+            <div class="row" id="loginform">
+                <div class="col-lg-12 p-5">
+                    <div class="row text-center">
                         <div class="col-12">
                             <h2 class="text-center">Iniciar sesión</h2>
                         </div>
@@ -23,17 +23,17 @@
                     <div class="row mt-3">
                         <div class="col-12" id="cuilcontainer">
                             <label for="email" id="labelcuil" class="form-label">Correo electrónico</label>
-                            <input type="text" name="email" id="email" class="form-control" placeholder="ejemplo@gmail.com" aria-label="email" required>
+                            <input type="text" name="email" id="email" class="form-control" placeholder="ejemplo@gmail.com" aria-label="email" style="border-radius: 15px;" required>
                         </div>
                     </div>
                     <div class="row mt-3 mb-3">
                         <div class="col-12" id="passwordcontainer">
                             <label for="password" id="labelpassword" class="form-label">Contraseña</label>
-                            <input type="password" name="password" id="password" class="form-control" placeholder="Ingrese su contraseña" aria-label="password" required>
+                            <input type="password" name="password" id="password" class="form-control" placeholder="Ingrese su contraseña" aria-label="password" style="border-radius: 15px;" required>
                         </div>
                     </div>
                     <div class="row mb-4">
-                        <div class="col-6">
+                        <div class="col-5">
                             <div id="recordarmediv">
                                 <label for="recordarme" class="form-label">
                                     <input type="checkbox" id="recordarme" name="recordarme" value="Recordarme">
@@ -43,13 +43,13 @@
                         </div>
                         <div class="col-6">
                             <div id="forgotpass">
-                                <p>¿Olvidaste tu contraseña?</p>
+                                <a href="#">¿Olvidaste tu contraseña?</a>
                             </div>
                         </div>
                     </div>
                     <div class="row mb-3">
-                        <div class="col-12">
-                            <button type="submit" class="btn" id="loginbutton" value="login" name="action"> 
+                        <div class="col-12 text-center">
+                            <button type="submit" class="btn" id="loginbutton" value="login" name="action" style="background-color: #d9db26; width: 30%; border-radius: 15px;"> 
                                 <h5>Ingresar</h5>
                             </button>
                         </div>
@@ -65,5 +65,5 @@
                 <!-- ------------------------------------- -->
             </div>
         </form>
-    </div>
+    </main>
 @endsection
